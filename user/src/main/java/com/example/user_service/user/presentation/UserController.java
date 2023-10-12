dc17e33085ffd69b368537ba2b288058c0fab7a5
goeom77 <7eom14@gmail.com>
2023-10-12 14:48:45 +0900

    Feat userservice annotation change

--- user/src/main/java/com/example/user_service/user/presentation/UserController.java
+++ user/src/main/java/com/example/user_service/user/presentation/UserController.java
@@ -3,14 +3,12 @@ package com.example.user_service.user.presentation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/v1")
 public class UserController {
     @GetMapping("/test")
     public String testUser() {
