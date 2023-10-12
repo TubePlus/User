bed52556c53bdff6c75b6353986da11035c02053
goeom77 <7eom14@gmail.com>
2023-10-12 11:02:44 +0900

    Feat userservice setting test

--- user/src/main/java/com/example/user_service/user/presentation/UserController.java
+++ user/src/main/java/com/example/user_service/user/presentation/UserController.java
@@ -2,11 +2,18 @@ package com.example.user_service.user.presentation;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
-
+    @GetMapping("/test")
+    public String testUser() {
+        return "성공적인 test";
+    }
 }
