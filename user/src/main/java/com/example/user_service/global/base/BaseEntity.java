f87f460aae02a91e4ff14bb29ec0467312be5a79
goeom77 <7eom14@gmail.com>
2023-10-17 11:41:11 +0900

    Feat userservice base setting

---
+++ user/src/main/java/com/example/user_service/global/base/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.example.user_service.global.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+}
